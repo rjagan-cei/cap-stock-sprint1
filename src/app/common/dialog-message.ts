5a8e68aafa6b28fe7d091eb3100879d4f8851279
Jagan <rjagan@ceiamerica.com>
2021-03-25 15:30:43 +0530

    confirmation dialog component added..

---
+++ src/app/common/dialog-message.ts
@@ -0,0 +1,1 @@
+export const CREATE_PROFILE_SAVE_DIALOG_MESSAGE = 'By Saving the member profile it will be submitted for Blind Verification. You will not be able to make any edits until verification is complete or fails';
